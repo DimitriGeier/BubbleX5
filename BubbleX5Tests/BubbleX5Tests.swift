d78cca6866c8fe876a2b18bfb4ccb0bbb78f05a8
Dimitri Geier <accounts.dg@icloud.com>
2025-11-30 20:00:33 -0500

    Initial Commit

---
+++ BubbleX5Tests/BubbleX5Tests.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleX5Tests.swift
+//  BubbleX5Tests
+//
+//  Created by Dimitri Geier on 11/30/25.
+//
+
+import Testing
+@testable import BubbleX5
+
+struct BubbleX5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
